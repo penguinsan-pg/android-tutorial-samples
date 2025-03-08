ff8965e84056bae241e85b5672141d5b15f03257
penguinsan <penguinsan.pg@gmail.com>
2025-03-08 19:23:06 +0900

    7. string resource に `happy_birthday_text` を追加

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,7 +33,7 @@ class MainActivity : ComponentActivity() {
             HappyBirthdayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     GreetingImage(
-                        message = "Happy Birthday Sam!",
+                        message = stringResource(R.string.happy_birthday_text),
                         from = "From Emma",
                     )
                 }
