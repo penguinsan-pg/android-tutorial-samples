a65603efa449ac300a08026f5bfd0ed71e50a058
penguinsan <penguinsan.pg@gmail.com>
2025-03-16 17:46:53 +0900

    3. 不要になった Greeting() を削除

--- TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
+++ TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
@@ -40,14 +40,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
 fun TaskCompleted(modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center ,
