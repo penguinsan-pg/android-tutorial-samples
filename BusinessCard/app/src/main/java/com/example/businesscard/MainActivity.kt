5d3c8bd3a3ff638e1b4d86951552dc643226016e
penguinsan <penguinsan.pg@gmail.com>
2025-03-22 19:41:24 +0900

    NavigationBar と UI が重ならないように、`.navigationBarsPadding()` を設定

--- BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -53,6 +54,7 @@ fun BusinessCard(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
             .fillMaxSize()
+            .navigationBarsPadding()
             .background(color = Color(0xFFD2E7D4))
     ) {
         BusinessCardMainArea(
