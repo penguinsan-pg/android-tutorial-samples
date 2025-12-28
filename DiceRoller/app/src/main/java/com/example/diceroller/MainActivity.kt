76b5a6eebdc1662ea5fb6a7bff24528f6332fdca
penguinsan <penguinsan.pg@gmail.com>
2025-12-28 13:50:10 +0900

    3. `Greeting()` 関数を削除

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -31,14 +31,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
 fun DiceWithButtonAndImage() {
     
 }
