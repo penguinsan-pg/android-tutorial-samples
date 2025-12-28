096271f6277fb6a20d077e8911d0a79cd2425a6a
penguinsan <penguinsan.pg@gmail.com>
2025-12-28 14:06:05 +0900

    3. modifierに `.wrapContentSize(Alignment.Center)` を追加

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
@@ -31,6 +33,7 @@ fun DiceRollerApp() {
     DiceWithButtonAndImage(
         modifier = Modifier
             .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
     )
 }
 
