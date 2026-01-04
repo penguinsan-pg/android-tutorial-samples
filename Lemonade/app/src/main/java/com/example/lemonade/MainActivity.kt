35ae7b908e3dd6e4d1263e14842edc51ed6e8b63
penguinsan <penguinsan.pg@gmail.com>
2026-01-04 13:58:32 +0900

    3. `setContent()` 内で LemonadeApp() を呼び出す

--- Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -35,12 +35,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LemonadeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LemonadeApp(
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
             }
         }
     }
