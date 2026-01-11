79378b9b5354793823402977f767c1b17f4d0280
penguinsan <penguinsan.pg@gmail.com>
2026-01-11 18:41:54 +0900

    5. `amountInput` の型をMutableStateに変更

--- basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -33,6 +33,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -84,9 +86,9 @@ fun TipTimeLayout() {
 
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
-    var amountInput = "0"
+    var amountInput = mutableStateOf("0")
     TextField(
-        value = amountInput,
+        value = amountInput.value,
         onValueChange = {},
         modifier = modifier
     )
