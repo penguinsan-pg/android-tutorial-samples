d01051b6f724e31a94ae71a7a71e07dc6dce32be
penguinsan <penguinsan.pg@gmail.com>
2026-01-17 15:42:52 +0900

    4. label引数に `@StringRes` アノテーションをつけ、labelが文字列リソースのIDを受け取ることを明示する

--- basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -19,6 +19,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -102,7 +103,7 @@ fun TipTimeLayout() {
 
 @Composable
 fun EditNumberField(
-    label: Int,
+    @StringRes label: Int,
     value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier
