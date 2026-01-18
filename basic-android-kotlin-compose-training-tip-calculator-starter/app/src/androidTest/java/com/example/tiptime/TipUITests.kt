a5f8a2fa5bb4694682593c6c545c3016699c1c08
penguinsan <penguinsan.pg@gmail.com>
2026-01-18 14:05:28 +0900

    5. Textが正しく表示されているかを確認するアサーションを追加

--- basic-android-kotlin-compose-training-tip-calculator-starter/app/src/androidTest/java/com/example/tiptime/TipUITests.kt
+++ basic-android-kotlin-compose-training-tip-calculator-starter/app/src/androidTest/java/com/example/tiptime/TipUITests.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.test.performTextInput
 import com.example.tiptime.ui.theme.TipTimeTheme
 import org.junit.Rule
 import org.junit.Test
+import java.text.NumberFormat
 
 class TipUITests {
 
@@ -21,5 +22,7 @@ class TipUITests {
         }
         composeTestRule.onNodeWithText("Bill Amount").performTextInput("10")
         composeTestRule.onNodeWithText("Tip Percentage").performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists("No node with this text was found.")
     }
 }
