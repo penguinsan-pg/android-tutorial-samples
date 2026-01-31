883ad36c677067f40e9de67e12a1890623c716ee
penguinsan <penguinsan.pg@gmail.com>
2026-01-31 19:57:02 +0900

    3. プレビュー表示用の `AffirmationCardPreview()` 関数を作成

--- basic-android-kotlin-compose-training-affirmations-starter/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ basic-android-kotlin-compose-training-affirmations-starter/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.affirmations.model.Affirmation
 import com.example.affirmations.ui.theme.AffirmationsTheme
@@ -79,4 +80,9 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
             )
         }
     }
+}
+
+@Preview
+@Composable
+private fun AffirmationCardPreview() {
 }
