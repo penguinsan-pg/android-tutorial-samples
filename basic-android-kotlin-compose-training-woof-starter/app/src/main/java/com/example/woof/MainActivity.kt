94c518ba383873176eb961e1f53cef124fec4940
penguinsan <penguinsan.pg@gmail.com>
2026-02-08 19:21:22 +0900

    7. `WoofTopAppBar()` を作成

--- basic-android-kotlin-compose-training-woof-starter/app/src/main/java/com/example/woof/MainActivity.kt
+++ basic-android-kotlin-compose-training-woof-starter/app/src/main/java/com/example/woof/MainActivity.kt
@@ -152,6 +152,10 @@ fun DogInformation(
     }
 }
 
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier) {
+}
+
 /**
  * Composable that displays what the UI of the app looks like in light theme in the design tab.
  */
