8ea645f09332529c0c23911d1a37c8363acd93e7
penguinsan <penguinsan.pg@gmail.com>
2026-02-11 18:19:04 +0900

    5. DogHobbyをif文で囲み、 `expanded == true` の場合のみ表示するように修正

--- basic-android-kotlin-compose-training-woof-starter/app/src/main/java/com/example/woof/MainActivity.kt
+++ basic-android-kotlin-compose-training-woof-starter/app/src/main/java/com/example/woof/MainActivity.kt
@@ -124,15 +124,17 @@ fun DogItem(
                     onClick = { expanded = !expanded },
                 )
             }
-            DogHobby(
-                dog.hobbies,
-                modifier = Modifier.padding(
-                    start = dimensionResource(R.dimen.padding_medium),
-                    top = dimensionResource(R.dimen.padding_small),
-                    end = dimensionResource(R.dimen.padding_medium),
-                    bottom = dimensionResource(R.dimen.padding_medium),
+            if (expanded) {
+                DogHobby(
+                    dog.hobbies,
+                    modifier = Modifier.padding(
+                        start = dimensionResource(R.dimen.padding_medium),
+                        top = dimensionResource(R.dimen.padding_small),
+                        end = dimensionResource(R.dimen.padding_medium),
+                        bottom = dimensionResource(R.dimen.padding_medium),
+                    )
                 )
-            )
+            }
         }
     }
 }
